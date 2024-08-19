886d1931be8994bb71b207c7fbfae91529966997
Rahul Pramanik <rahulpramanik@gmail.com.com>
2024-08-19 16:49:55 +0530

    Chnages

--- src/main/java/com/asb/example/SpringKafkaSynchronousExampleApplication.java
+++ src/main/java/com/asb/example/SpringKafkaSynchronousExampleApplication.java
@@ -3,6 +3,7 @@ package com.asb.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 
 @SpringBootApplication
 public class SpringKafkaSynchronousExampleApplication {
